f6bdf60fb916fec5ed59285a557008ebb038fdfb
Karik Isichei <karik.isichei@gmail.com>
2018-09-03 14:50:02 +0100

    testing new setup script to install requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(
     license='MIT',
     description='A python package to manage etl processes on AWS',
     long_description=open('README.md').read(),
-    install_requires=[],
+    install_requires=[
+        "boto3 >= 1.7.4",
+        "PyAthenaJDBC >= 1.3.0",
+        "jsonschema >= 2.6.0"
+    ],
     include_package_data=True,
     url='https://github.com/moj-analytical-services/etl_manager',
     author='Karik Isichei',
