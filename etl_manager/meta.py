02dc80cda32d7c8c8eee989109dd475990271bd0
nicholsondarius <darius.nicholson@digital.justice.gov.uk>
2020-08-05 16:32:28 +0100

    Allow upper case string in location setter

--- etl_manager/meta.py
+++ etl_manager/meta.py
@@ -179,7 +179,7 @@ class TableMeta:
 
     @location.setter
     def location(self, location):
-        _validate_string(location, allowed_chars="_/-")
+        _validate_string(location, allowed_chars="_/-", allow_upper=True)
         if location and location != "":
             if location[0] == "/":
                 raise ValueError("location should not start with a slash")
