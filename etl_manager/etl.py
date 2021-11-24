e1ef225cd518d71f9bd2fdd6fc3f25923b5fc98a
Anthony Cody <anthonycody01@gmail.com>
2021-11-24 17:43:14 +0000

    Revert "Allow tags to be set on Glue jobs (#142)"

--- etl_manager/etl.py
+++ etl_manager/etl.py
@@ -1,17 +1,26 @@
+from urllib.request import urlretrieve
 import datetime
 import glob
+import json
 import os
 import re
 import shutil
 import tempfile
 import time
-from typing import Optional
 import zipfile
-from urllib.request import urlretrieve
-
 from botocore.exceptions import ClientError
 
-from etl_manager.utils import _glue_client, _s3_client, _s3_resource, _validate_string
+from etl_manager.utils import (
+    read_json,
+    write_json,
+    _dict_merge,
+    _validate_string,
+    _glue_client,
+    _unnest_github_zipfile_and_return_new_zip_path,
+    _s3_client,
+    _s3_resource,
+)
+
 
 # Create temp folder - upload to s3
 # Lock it in
@@ -82,7 +91,6 @@ class GlueJob:
         job_arguments={},
         include_shared_job_resources=True,
         timeout_override_minutes=None,
-        tags: Optional[dict] = None,
     ):
         self.GLUE_WORKER_HOURLY_COST = 0.44  # i.e. 44 cents per worker per hour
         self.MAXIMUM_COST_TIMEOUT = (
@@ -121,7 +129,6 @@ class GlueJob:
         self.job_arguments = job_arguments
 
         self.timeout_override_minutes = timeout_override_minutes
-        self.tags = tags if tags is not None else dict()
 
         self.github_py_resources = []
 
@@ -230,15 +237,10 @@ class GlueJob:
         _validate_string(job_name, allowed_chars="-_:")
         self._job_name = job_name
 
-    @property
-    def tags(self):
-        return self._tags
-
-    @tags.setter
-    def tags(self, tags: dict):
-        if not isinstance(tags, dict):
-            raise TypeError("tags must be of type dict")
-        self._tags = tags
+    @job_name.setter
+    def job_name(self, job_name):
+        _validate_string(job_name, allowed_chars="-_:")
+        self._job_name = job_name
 
     @property
     def job_run_id(self):
@@ -493,7 +495,6 @@ class GlueJob:
             "MaxRetries": self.max_retries,
             "AllocatedCapacity": self.allocated_capacity,
             "GlueVersion": self.glue_version,
-            "Tags": self.tags,
             "Timeout": self.timeout,
         }
 
